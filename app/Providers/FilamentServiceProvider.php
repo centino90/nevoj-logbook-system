f5de6d5bf04a18db44c4483f728c518b67a9d856
centino90 <anthonyjayansit2326282@gmail.com>
2023-01-13 00:33:20 +0800

    add filament-auth, policies, env

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -3,8 +3,13 @@
 namespace App\Providers;
 
 use Filament\Facades\Filament;
+use Filament\Navigation\NavigationItem;
+use Filament\Navigation\UserMenuItem;
 use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
+use Phpsa\FilamentAuthentication\Resources\UserResource;
+use Phpsa\FilamentAuthentication\Resources\RoleResource;
+use Phpsa\FilamentAuthentication\Resources\PermissionResource;
 
 class FilamentServiceProvider extends ServiceProvider
 {
@@ -27,5 +32,25 @@ class FilamentServiceProvider extends ServiceProvider
         Filament::serving(function() {
             Filament::registerViteTheme('resources/css/filament.css');
         });
+
+
+        // if (auth()->user()) {
+        //     if (auth()->user()->is_admin === 1 && auth()->user()->hasAnyRole(['super-admin', 'admin', 'moderator'])) {
+        //         Filament::registerUserMenuItems([
+        //             UserMenuItem::make()
+        //                 ->label('Manage Users')
+        //                 ->url(UserResource::getUrl())
+        //                 ->icon('heroicon-s-users'),
+        //             UserMenuItem::make()
+        //                 ->label('Manage Roles')
+        //                 ->url(RoleResource::getUrl())
+        //                 ->icon('heroicon-s-cog'),
+        //             UserMenuItem::make()
+        //                 ->label('Manage Permissions')
+        //                 ->url(PermissionResource::getUrl())
+        //                 ->icon('heroicon-s-key'),
+        //         ]);
+        //     }
+        // }
     }
 }
---
+++ database/seeders/UserAndRoleSeeder.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+
+class UserAndRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // CREATE ROLES
+        $adminRole = Role::where('name', 'admin')->first();
+        if(!$adminRole) {
+            $adminRole = Role::create(['name' => 'admin', 'guard_name' => 'web']);
+        }
+        $professorRole = Role::where('name', 'professor')->first();
+        if(!$professorRole) {
+            $professorRole = Role::create(['name' => 'professor', 'guard_name' => 'web']);
+        }
+
+        // CREATE 1 ADMIN and 2 PROFESSOR USERS
+
+        if(User::whereHas('roles', function($q) {
+            $q->where('name', 'admin');
+        })->count() === 0) {
+            User::create([
+                'name' => 'admin',
+                'email' => 'admin@test.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ])->assignRole($adminRole);
+        }
+
+        if(User::whereHas('roles', function($q) {
+            $q->where('name', 'professor');
+        })->count() === 0) {
+            User::create([
+                'name' => 'professor one',
+                'email' => 'professor1@test.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ])->assignRole($professorRole);
+            User::create([
+                'name' => 'professor two',
+                'email' => 'professor2@test.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ])->assignRole($professorRole);
+        }
+    }
+}
